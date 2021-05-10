3a7d90e2a7605fe7e75b43a58b58216dd70ea1d6
andresmunozdesign <munoz_andres_adrian@student.smc.edu>
2021-05-10 09:44:07 -0700

    try this

--- week_11/Homework/canvasGame.js
+++ week_11/Homework/canvasGame.js
@@ -11,10 +11,10 @@ let cPosY = cHeight/5;
 let cVelX = 3;
 let cVelY = 2;
 let cRadius = 75;
-let colorPicker = "green";
+let colorPicker = "red";
 
-let score = 0;
-let scoreBoard = "YOUR SCORE"+ score;
+let sc0re = 0;
+let scoreBoard = "YOUR SCORE" + sc0re;
 
 
 
@@ -60,13 +60,27 @@ canvas2.addEventListener("click", function(event){
         let distY = Math.abs(cPosY - mouseYp);
 
         if(distX < cRadius & distY < cRadius) {
-        
-
+     
         cVelX = cVelX * 2;
         cVelY = cVelY * 2;
+		
         }
+	
+	
 
 
 })
 
-
+jscolor.presets.default = {
+  palette: [
+    '#000000', '#7d7d7d', '#870014', '#ec1c23', '#ff7e26', '#fef100', '#22b14b', '#00a1e7', '#3f47cc', '#a349a4',
+    '#ffffff', '#c3c3c3', '#b87957', '#feaec9', '#ffc80d', '#eee3af', '#b5e61d', '#99d9ea', '#7092be', '#c8bfe7',
+  ],
+  //paletteCols: 12,
+  //hideOnPaletteClick: true,
+  //width: 271,
+  //height: 151,
+  //position: 'right',
+  //previewPosition: 'right',
+  //backgroundColor: 'rgba(51,51,51,1)', controlBorderColor: 'rgba(153,153,153,1)', buttonColor: 'rgba(240,240,240,1)',
+}
