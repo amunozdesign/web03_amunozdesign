ad58c7d70e4b44ce152fa89f471b3790a0fc11db
andresmunozdesign <munoz_andres_adrian@student.smc.edu>
2021-05-17 12:01:02 -0700

    upload hw

--- week_11/in_class/canvasGame.js
+++ week_11/in_class/canvasGame.js
@@ -4,57 +4,89 @@ const ctx = canvas2.getContext('2d');
 let cWidth = canvas2.width;
 let cHeight = canvas2.height;
 
-let cPosX = cWidth/2.5;
-let cPosY = cHeight/5;
+let cPosX = cWidth/2; //horizontal center
+let cPosY = cHeight/2; //vertical center
 
-let cVelX = 3;
-let cVelY = 2;
+let cVelX = 2;
+let cVelY = 1;
+
 let cRadius = 75;
+let ballColor = "rgb(0, 0, 0)";
 
-function drawFrame(){
-  ctx.clearRect(0, 0, cWidth, cHeight);
-  ctx.fillStyle = "green";
-  ctx.strokeStyle = "darkorange";
-  
-  ctx.beginPath();
-  ctx.arc(cPosX, cPosY, cRadius, 0, Math.PI*2);
-  ctx.fill();
-  ctx.stroke();
-  ctx.closePath();
+const redSlider = document.getElementById("redValue");
+const greenSlider = document.getElementById("greenValue");
+const blueSlider = document.getElementById("blueValue");
 
-  if(cPosX + cRadius >= cWidth || cPosX - cRadius <= 0) {
-      cVelX = cVelX * -1; //reverse the horizontal directions
+let randomColor = function(){
+    let randR = Math.random()*255;
+    let randG = Math.random()*255;
+    let randB = Math.random()*255;
 
-  }
-  if(cPosY + cRadius >= cHeight  || cPosY - cRadius <= 0) {
-    cVelY = cVelY * -1; //reverse the Vertical directions
-
+    let colValue = "rgb(" + randR + ", "+ randG +" , " + randB + ")";
+    return colValue;
 }
 
-  cPosX = cPosX + cVelX;
-  cPosY = cPosY + cVelY;
+let calculateColor = function(){
+    let valueR = redSlider.value;
+    let valueG = greenSlider.value;
+    let valueB = blueSlider.value;
 
-window.requestAnimationFrame(drawFrame);
+    let colValue = "rgb(" + valueR + ", " + valueG + ", " + valueB + ")";
+    ballColor = colValue;
+}
+
+
+function drawFrame(){
+   ctx.clearRect(0, 0, cWidth, cHeight);
+
+   // if re-generating every frame:
+   // ballColor = randomColor();
+
+    ctx.fillStyle = ballColor; // define as: #FF0CE2 , rgb(255, 128, 35), "wheat"
+    ctx.strokeStyle = "darkorange";
+
+    ctx.beginPath();
+    ctx.arc(cPosX, cPosY, cRadius, 0, Math.PI*2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    if(cPosX + cRadius >= cWidth || cPosX - cRadius <= 0) {
+        cVelX = cVelX * -1; //reverse the horizontal direction
+    }
+    if(cPosY + cRadius >= cHeight || cPosY - cRadius <= 0){
+        cVelY = cVelY * -1;
+    }
+
+    cPosX = cPosX + cVelX; // change position by velocity
+    cPosY = cPosY + cVelY; //
+
+    window.requestAnimationFrame(drawFrame);
 }
 
 drawFrame();
 
+
 canvas2.addEventListener("click", function(event){
-        //console.log(event);
-        let mouseXp = event.pageX - event.target.offsetLeft;
-        let mouseYp = event.pageY - event.target.offsetTop;
+    //console.log(event);
+    let mouseXp = event.pageX - event.target.offsetLeft;
+    let mouseYp = event.pageY - event.target.offsetTop;
+    //console.log("Mouse X: " + mouseXp + " Mouse Y: " + mouseYp);
 
-        //console.log("Mouse X: " + mouseXp + " Mouse Y: " + mouseYp);
+    let distX = Math.abs(cPosX - mouseXp);
+    let distY = Math.abs(cPosY - mouseYp);
 
-        let distX = Math.abs(cPosX - mouseXp);
-        let distY = Math.abs(cPosY - mouseYp);
-
-        if(distX < cRadius & distY < cRadius){
-        console.log("HIT!!!!!!");
-        cVelX = cVelX * 2;
-        cVelY = cVelY * 2;
-
-        }
-
-
+    if(distX < cRadius && distY < cRadius){
+        console.log("HIT!!!");
+        cVelX = cVelX * 1.5;
+        cVelY = cVelY * 1.5;
+    }
 })
+
+document.getElementById("colorRand").addEventListener("click", function(){
+    ballColor = randomColor();
+})
+
+redSlider.addEventListener("change", calculateColor);
+greenSlider.addEventListener("change", calculateColor);
+blueSlider.addEventListener("change", calculateColor);
